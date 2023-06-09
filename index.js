a2a6e1c37df1d6e13715c011625e368c85b1a630
Edgar Nwajei <edgarlinked@gmail.com>
2023-06-09 15:19:15 +0100

    toggle function

---
+++ index.js
@@ -0,0 +1,12 @@
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelectorAll("body");
+
+
+toggle.addEventListener("click", function(){
+this.classList.toggle('bi-moon');
+if (this.classList.toggle('bi bi-brightness-high-fill')) {
+    body.style.background =
+}
+
+
+})
